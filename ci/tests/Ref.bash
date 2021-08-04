2310512ff9101e79b3f1900451177fb44b906574
r9-pena <r9_pena@hotmail.com>
2021-08-04 09:43:47 -0600

    Test entrypoint #4

--- ci/tests/Ref.bash
+++ ci/tests/Ref.bash
@@ -22,6 +22,7 @@ echo "Bash Source: ${BASH_SOURCE}"
 echo "FPRIME_DIR: ${FPRIME_DIR}"
 echo "LOG_DIR: ${LOG_DIR}"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+${SCRIPT_DIR} ls
 
 echo -e "${BLUE}Starting CI test ${FPUTIL_DEPLOYS} Ref${NOCOLOR}"
 
